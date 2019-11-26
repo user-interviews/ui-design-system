f44ab43f80f8d349d975681ca42bfeb6a7b13e7a
Dave Ferris <dave@userinterviews.com>
2019-11-26 15:49:57 -0800

    Add Global styling and Card component

---
+++ src/Card.stories.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+
+import Card from './Card';
+
+export default {
+  title: 'Design System|Card',
+  component: Card,
+  decorators: [withKnobs],
+};
+
+export const allCards = () => (
+  <div>
+    <Card title="Test Card">
+      <div>
+        {
+          text('Card text', 'Some quick example text to build on the card title and make up the bulk of the card\'s content.')
+        }
+      </div>
+    </Card>
+  </div>
+);
+
+allCards.story = {
+  name: 'all cards',
+};
---
+++ src/shared/styles.js
@@ -0,0 +1,35 @@
+export const background = {
+};
+
+export const color = {
+  // Palette
+  primary: '#6DBD63', // green
+  secondary: '#337ab7', // blue
+  lightest: '#FFFFFF',
+};
+
+export const spacing = {
+};
+
+export const typography = {
+  type: {
+    primary: 'Lato, sans-serif',
+  },
+  weight: {
+    regular: '300',
+    bold: '400',
+    extrabold: '700',
+  },
+  size: {
+    s1: '12',
+    s2: '14',
+    s3: '16',
+    m1: '20',
+    m2: '24',
+    m3: '28',
+    l1: '32',
+    l2: '40',
+    l3: '48',
+    code: '90',
+  },
+};
