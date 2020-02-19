25cc02c0f9616eb6142e2581aa4121ade2d78d6c
Dave Ferris <dave@userinterviews.com>
2020-02-19 12:48:15 -0800

    Add custom webpack config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = function({ config }) {
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../')
+  });
+
+  return config;
+};
