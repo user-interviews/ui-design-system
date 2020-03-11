9332dba4878902a1bd2b5aa838b24fab34fcd985
Bob Saris <bob@bobsaris.com>
2020-03-11 14:52:11 -0400

    CHORE Add z_stack styles and JS add colors to JS
    This brings over the z stack variables and introduces color variables to be used
    for styled components as well as adding similar variables for z stack.

---
+++ src/Styles/zStack.js
@@ -0,0 +1,26 @@
+// This can probably be generated... see work on palette.js
+
+export default {
+  zIndex1045: 1045,
+  zIndex1040: 1040,
+  zIndex1030: 1030,
+  zIndex1025: 1025,
+  zIndex1020: 1020,
+
+  zIndex115: 115,
+  zIndex100: 100,
+
+  zIndex30: 30,
+  zIndex20: 20,
+  zIndex10: 10,
+  zIndex3: 3,
+  zIndex2: 2,
+  zIndex1: 1,
+
+  // By Purpose
+  zIndexFlash: 1045,
+  zIndexDrawer: 1040,
+  zIndexModalBackdrop: 1040, // Matches bootstrap $zindex-modal-backdrop
+  zIndexFixed: 1030, // Matches bootstrap $z-index-fixed
+  zIndexOverlay: 1029, // Sits just under $z-index-fixed
+};
