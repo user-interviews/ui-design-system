fa2cef02fad5645aebbe250ee71c8223e6f70708
Robert Ang <angrobertsh@gmail.com>
2020-04-08 19:24:30 -0700

    Update src/Flash/AlertMessage.jsx
    Co-Authored-By: Bob Saris <bob@userinterviews.com>

--- src/Flash/AlertMessage.jsx
+++ src/Flash/AlertMessage.jsx
@@ -30,7 +30,7 @@ export default function AlertMessage(props) {
             type="button"
             onClick={handleDismissMessage}
           >
-          &times;
+            &times;
           </button>
         )
       }
