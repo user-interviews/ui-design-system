ad3ad980ec74fbc395a9ede52f95e072d74e4f2e
Rachel Roppolo <rroppolo@Rachels-MacBook-Pro.local>
2020-11-30 18:55:53 -0800

    UIDS-87 Update RadioButton component to move label outside to leverage new styles

--- src/RadioButton/RadioButton.jsx
+++ src/RadioButton/RadioButton.jsx
@@ -1,52 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-
-import './RadioButton.scss';
 
 export default function RadioButton(props) {
-  const classNames = classnames('RadioButton', {
-    'RadioButton--bordered': props.bordered,
-  });
-
   return (
-    <label className={classNames}>
+    <Fragment>
       <input
-        className="RadioButton__input"
+        className={props.className}
         disabled={props.disabled}
         id={props.id}
         name={props.name}
         type="radio"
-        {...props.radioButtonProps}
+        {...props}
       />
-
-      {props.label && (
-        <span className="RadioButton__label">{props.label}</span>
-      )}
-
-      {props.children && (
-        <div className="RadioButton__children">
-          {props.children}
-        </div>
-      )}
-    </label>
+    </Fragment>
   );
 }
 
 RadioButton.propTypes = {
-  bordered: PropTypes.bool,
-  children: PropTypes.node,
+  className: PropTypes.string,
   disabled: PropTypes.bool,
   id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
   name: PropTypes.string,
-  radioButtonProps: PropTypes.object,
 };
 
 RadioButton.defaultProps = {
-  bordered: false,
-  children: null,
+  className: undefined,
   disabled: false,
   name: '',
-  radioButtonProps: {},
 };
