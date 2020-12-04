e805dd1e4fcd14cacf02652a08f0ee3dc11ee813
Rachel Roppolo <rachel@userinterviews.com>
2020-12-04 12:37:56 -0800

    CHORE Add forwardRef to RadioButton and some styling tweaks (#98)

--- src/RadioButton/RadioButton.jsx
+++ src/RadioButton/RadioButton.jsx
@@ -12,12 +12,15 @@ const RadioButton = React.forwardRef((props, ref) => {
   return (
     <label className={classNames}>
       <input
+        checked={props.checked}
         className="RadioButton__input"
         disabled={props.disabled}
         id={props.id}
         name={props.name}
         ref={ref}
         type="radio"
+        value={props.value}
+        onChange={props.onChange}
         {...props.radioButtonProps}
       />
 
@@ -36,20 +39,32 @@ const RadioButton = React.forwardRef((props, ref) => {
 
 RadioButton.propTypes = {
   bordered: PropTypes.bool,
+  checked: PropTypes.bool,
   children: PropTypes.node,
   disabled: PropTypes.bool,
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   name: PropTypes.string,
   radioButtonProps: PropTypes.object,
+  value: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.bool,
+  ]),
+  onChange: PropTypes.func,
 };
 
 RadioButton.defaultProps = {
   bordered: false,
+  checked: undefined,
   children: null,
   disabled: false,
   name: '',
   radioButtonProps: {},
+  value: undefined,
+  onChange: undefined,
 };
+
+RadioButton.displayName = 'RadioButton';
 
 export default RadioButton;
