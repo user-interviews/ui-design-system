70059201428f1d7be9ae0c083f22d8426e6bb560
Jamison Ordway <33180544+jamisonordway@users.noreply.github.com>
2020-12-09 18:14:37 -0700

    UIDS-100 create CopyToClipboardButton (#101)

--- src/CopyToClipboard/CopyToClipboard.jsx
+++ src/CopyToClipboard/CopyToClipboard.jsx
@@ -1,40 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-regular-svg-icons';
-import { CopyToClipboard as ReactCopyToClipboard } from 'react-copy-to-clipboard';
-
-import Popper from 'src/Popper';
-import TrackedButton from 'src/TrackedButton';
+import CopyToClipboardButton, { ButtonVariants } from 'src/CopyToClipboardButton';
 
 import './CopyToClipboard.scss';
 
 function CopyToClipboard(props) {
-  const [copied, setCopied] = useState(false);
-
-  const handleClickCopy = () => {
-    setCopied(true);
-    setTimeout(() => setCopied(false), 1000);
-  };
-
   return (
     <div className="CopyToClipboard">
       <span className="CopyToClipboard__copy-text">{props.copyText}</span>
-      <ReactCopyToClipboard text={props.copyText} onCopy={handleClickCopy}>
-        <TrackedButton
-          className="btn btn-link btn-link--neutral"
-          event={props.trackingEvent}
-          type="button"
-        >
-          <Popper
-            dark
-            text="Copied!"
-            visible={copied}
-          >
-            <div><FontAwesomeIcon icon={faCopy} /></div>
-          </Popper>
-        </TrackedButton>
-      </ReactCopyToClipboard>
+      <CopyToClipboardButton
+        copyText={props.copyText}
+        trackingEvent={props.trackingEvent}
+        variant={ButtonVariants.NEUTRAL}
+      />
     </div>
   );
 }
