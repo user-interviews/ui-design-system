0173acd09e37c618c8dbda2460e94bd257d757ec
Jason Basuil <basuilj@gmail.com>
2021-02-22 11:13:21 -0800

    adds icons to AlertMessage

--- src/Alert/AlertMessage.jsx
+++ src/Alert/AlertMessage.jsx
@@ -1,23 +1,46 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faBullhorn, faCheckCircle, faExclamationTriangle, faInfoCircle 
+} from '@fortawesome/free-solid-svg-icons';
+
 import './AlertMessage.scss';
 
 export const MessageTypes = {
   SUCCESS: 'success',
   INFO: 'info',
+  ANNOUNCEMENT: 'announcement',
   WARNING: 'warning',
   ERROR: 'error',
 };
 
 const AUTO_DISMISS_TIMEOUT = 5000;
 
-const alertMessageClassName = (type) => {
+const getAlertMessageClassName = (type) => {
   if (!Object.values(MessageTypes).includes(type)) {
-    throw new TypeError(`Unexpected type ${type} used for a alert message.`);
+    throw new TypeError(`Unexpected type ${type} used for an alert message.`);
   }
 
   return `AlertMessage AlertMessage-${type}`;
+};
+
+const getAlertMessageIcon = (type) => {
+  switch(type) {
+    case 'success':
+      return faCheckCircle;
+    case 'info':
+      return faInfoCircle;
+    case 'announcement':
+      return faBullhorn;
+    case 'warning':
+      return faExclamationTriangle;
+    case 'error':
+      return faExclamationTriangle;
+    default:
+      return null;
+  }
 };
 
 function AlertMessage(props) {
@@ -34,24 +57,33 @@ function AlertMessage(props) {
   }, [autoDismiss, onDismiss, id]);
 
   return (
-    <div className={alertMessageClassName(props.type)}>
-      {
-        props.onDismiss && (
-          <button
-            className="close"
-            type="button"
-            onClick={() => props.onDismiss(props.id)}
-          >
-            &times;
-          </button>
-        )
-      }
+    <div className={getAlertMessageClassName(props.type)}>
+      <div className="AlertMessage__icon">
+        <FontAwesomeIcon icon={getAlertMessageIcon(props.type)} />  
+      </div>
+      <div className="AlertMessage__content">
       {
         props.title && (
-          <p className="AlertMessage__title">{props.title}</p>
+          <p className="AlertMessage__title">
+            {props.title}
+          </p>
         )
       }
       {props.message}
+      </div>
+      {
+        props.onDismiss && (
+          <div className="AlertMessage__close">
+            <button
+              className="close"
+              type="button"
+              onClick={() => props.onDismiss(props.id)}
+            >
+              &times;
+            </button>
+          </div>
+        )
+      }
     </div>
   );
 }
