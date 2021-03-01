d03ed7c3c3b7824ec78f8f3915ede8938cb9390a
Jason Basuil <basuilj@gmail.com>
2021-03-01 10:56:58 -0800

    add NavLink, react-router-dom dependency

--- src/SidebarNav/SidebarNavLink.jsx
+++ src/SidebarNav/SidebarNavLink.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import { NavLink } from 'common/routing';
 
 import './SidebarNavLink.scss';
 
