4e35d03a812c3994f868aed2755f2565932ca061
Jason Basuil <basuilj@gmail.com>
2021-03-03 14:58:28 -0800

    add ModalHeader

---
+++ src/Modal/ModalHeader.jsx
@@ -0,0 +1,67 @@
+import React, { Component, Fragment } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import './ModalHeader.scss';
+
+export default class ModalHeader extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    closingIsDisabled: PropTypes.bool,
+    subtitle: PropTypes.string,
+    title: PropTypes.string,
+    titleClass: PropTypes.string,
+    titleId: PropTypes.string.isRequired,
+    onRequestClose: PropTypes.func,
+  };
+
+  static defaultProps = {
+    children: undefined,
+    closingIsDisabled: false,
+    title: undefined,
+    titleClass: '',
+    subtitle: undefined,
+    onRequestClose: undefined,
+  };
+
+  // Don’t pass event to props callback; the callback is not always called from
+  // event listeners:
+
+  handleCloseClick = () => this.props.onRequestClose();
+
+  get titleClassName() {
+    return classNames('ModalHeader__title', this.props.titleClass);
+  }
+
+  render() {
+    return (
+      <header className="ModalHeader">
+        <div className="ModalHeader__heading">
+          {this.props.children ?
+            this.props.children : (
+              <Fragment>
+                <h1 className={this.titleClassName} id={this.props.titleId}>
+                  {this.props.title}
+                </h1>
+                {this.props.subtitle && (
+                  <h2 className="ModalHeader__subtitle">
+                    {this.props.subtitle}
+                  </h2>
+                )}
+              </Fragment>
+            )}
+        </div>
+        {this.props.onRequestClose && (
+          <button
+            aria-label="Close"
+            className="btn btn-sm btn-link close"
+            disabled={this.props.closingIsDisabled}
+            type="button"
+            onClick={this.handleCloseClick}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        )}
+      </header>
+    );
+  }
+}
