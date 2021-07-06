750ee7ed0584a0e12f4b0b2fc0197ef7eb183ab0
Rachel Roppolo <rroppolo@Rachels-MacBook-Pro.local>
2021-07-06 10:07:38 -0400

    UIDS-314 Update radio button stories to reflect different variants

---
+++ src/RadioButtonGroup/RadioButtonGroup.stories.jsx
@@ -0,0 +1,262 @@
+import React, { useState } from 'react';
+import {
+  boolean, radios, text, withKnobs,
+} from '@storybook/addon-knobs';
+import { ORIENTATIONS } from '../ControlButtonGroup';
+import RadioButtonGroup from './index';
+import RadioButton from '../RadioButton';
+import FormControlLabel from '../FormControlLabel';
+import FormGroup from '../FormGroup/FormGroup';
+import mdx from './RadioButtonGroup.mdx';
+
+export default {
+  title: 'Design System/Form Elements/RadioButtonGroup',
+  component: RadioButtonGroup,
+  decorators: [withKnobs()],
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+};
+
+/* eslint-disable react/prop-types */
+const RadioButtonGroupComponent = ({
+  bordered,
+  children,
+  defaultValue,
+  fullWidth,
+  id,
+  inline,
+  label,
+  labelHelperText,
+  orientation,
+  labelHtmlFor,
+}) => {
+  const [value, setValue] = useState(defaultValue);
+
+  return (
+    <FormGroup
+      bordered={bordered}
+      id={id}
+      inline={inline}
+      label={label}
+      labelHelperText={labelHelperText}
+      labelHtmlFor={labelHtmlFor}
+    >
+      <RadioButtonGroup
+        fullWidth={fullWidth}
+        id="button-group"
+        orientation={orientation}
+        value={value}
+        onChange={setValue}
+      >
+        { children }
+      </RadioButtonGroup>
+    </FormGroup>
+  );
+};
+/* eslint-enable react/prop-types */
+
+export const Default = () => {
+  const orientation = radios(
+    'Orientation',
+    Object.values(ORIENTATIONS),
+    ORIENTATIONS.COLUMN,
+  );
+  const bordered = boolean('Bordered buttons', false);
+
+  return (
+    <RadioButtonGroupComponent
+      bordered={boolean('Bordered Form Group', false)}
+      defaultValue={[]}
+      fullWidth={boolean('Full width', false)}
+      id="with-radio-button-group"
+      label="Label"
+      labelHelperText="use the knobs to try out different variations"
+      labelHtmlFor="radio-button-group"
+      orientation={orientation}
+    >
+      <FormControlLabel
+        bordered={bordered}
+        Control={RadioButton}
+        id="value-1"
+        text={text('Label 1', 'Value 1')}
+        value="1"
+      />
+      <FormControlLabel
+        bordered={bordered}
+        Control={RadioButton}
+        id="value-2"
+        text={text('Label 2', 'Value 2')}
+        value="2"
+      />
+      <FormControlLabel
+        bordered={bordered}
+        Control={RadioButton}
+        id="value-3"
+        text={text('Label 3', 'Value 3')}
+        value="3"
+      />
+    </RadioButtonGroupComponent>
+  );
+};
+
+export const DefaultRow = () => (
+  <RadioButtonGroupComponent
+    bordered={false}
+    defaultValue={[]}
+    fullWidth={false}
+    id="with-radio-button-group"
+    inline
+    label="Label"
+    labelHelperText="helper text"
+    labelHtmlFor="radio-button-group"
+    orientation={ORIENTATIONS.ROW}
+  >
+    <FormControlLabel
+      bordered={false}
+      Control={RadioButton}
+      id="value-1"
+      text="Label 1"
+      value="1"
+    />
+    <FormControlLabel
+      bordered={false}
+      Control={RadioButton}
+      id="value-2"
+      text="Label 2"
+      value="2"
+    />
+    <FormControlLabel
+      bordered={false}
+      Control={RadioButton}
+      id="value-3"
+      text="Label 3"
+      value="3"
+    />
+  </RadioButtonGroupComponent>
+);
+
+export const BorderedRow = () => (
+  <RadioButtonGroupComponent
+    bordered={false}
+    defaultValue={[]}
+    fullWidth={false}
+    id="with-radio-button-group"
+    label="Label"
+    labelHelperText="helper text"
+    labelHtmlFor="radio-button-group"
+    orientation={ORIENTATIONS.ROW}
+  >
+    <FormControlLabel
+      bordered
+      Control={RadioButton}
+      id="value-1"
+      text="Label 1"
+      value="1"
+    />
+    <FormControlLabel
+      bordered
+      Control={RadioButton}
+      id="value-2"
+      text="Label 2"
+      value="2"
+    />
+    <FormControlLabel
+      bordered
+      Control={RadioButton}
+      id="value-3"
+      text="Label 3"
+      value="3"
+    />
+  </RadioButtonGroupComponent>
+);
+
+export const Description = () => (
+  <RadioButtonGroupComponent
+    bordered={false}
+    defaultValue={[]}
+    fullWidth
+    id="with-radio-button-group"
+    label="Label"
+    labelHelperText="helper text"
+    labelHtmlFor="radio-button-group"
+    orientation={ORIENTATIONS.COLUMN}
+  >
+    <FormControlLabel
+      bordered
+      Control={RadioButton}
+      helperText="helper text"
+      id="value-1"
+      text="Label 1"
+      value="1"
+    >
+      This is where the description goes
+    </FormControlLabel>
+    <FormControlLabel
+      bordered
+      Control={RadioButton}
+      helperText="helper text"
+      id="value-2"
+      text="Label 2"
+      value="2"
+    >
+      This is where the description goes
+    </FormControlLabel>
+    <FormControlLabel
+      bordered
+      Control={RadioButton}
+      helperText="helper text"
+      id="value-3"
+      text="Label 3"
+      value="3"
+    >
+      This is where the description goes
+    </FormControlLabel>
+  </RadioButtonGroupComponent>
+);
+
+export const DescriptionRow = () => (
+  <RadioButtonGroupComponent
+    bordered={false}
+    defaultValue={[]}
+    fullWidth
+    id="with-radio-button-group"
+    label="Label"
+    labelHelperText="helper text"
+    labelHtmlFor="radio-button-group"
+    orientation={ORIENTATIONS.ROW}
+  >
+    <FormControlLabel
+      bordered
+      Control={RadioButton}
+      helperText="helper text"
+      id="value-1"
+      text="Label 1"
+      value="1"
+    >
+      This is where the description goes
+    </FormControlLabel>
+    <FormControlLabel
+      bordered
+      Control={RadioButton}
+      helperText="helper text"
+      id="value-2"
+      text="Label 2"
+      value="2"
+    >
+      This is where the description goes
+    </FormControlLabel>
+    <FormControlLabel
+      bordered
+      Control={RadioButton}
+      helperText="helper text"
+      id="value-3"
+      text="Label 3"
+      value="3"
+    >
+      This is where the description goes
+    </FormControlLabel>
+  </RadioButtonGroupComponent>
+);
