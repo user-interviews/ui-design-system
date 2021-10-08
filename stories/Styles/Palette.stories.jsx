647ff37b17eefeca7a68ad15f3d8e2b95d54a465
Jason Basuil <basuilj@gmail.com>
2021-10-08 10:11:34 -0700

    updates semantic colors and fills in remaining brand colors

--- stories/Styles/Palette.stories.jsx
+++ stories/Styles/Palette.stories.jsx
@@ -22,12 +22,14 @@ const Palette = ({ color }) => (
               style={{
                 backgroundColor: '#FFF',
                 color: '#000',
-                fontSize: '1rem',
+                fontSize: '0.75rem',
                 margin: '1rem',
+                padding: '0.25rem',
                 textAlign: 'center',
               }}
             >
-              {colorNameKey}
+              <div>{colorNameKey}</div>
+              <div>{colors[colorNameKey]}</div>
             </p>
           </div>
         );
@@ -47,6 +49,7 @@ export const Gray = () => <Palette color="GRAY" />;
 export const Green = () => <Palette color="GREEN" />;
 export const Orange = () => <Palette color="ORANGE" />;
 export const Red = () => <Palette color="RED" />;
+export const Yellow = () => <Palette color="YELLOW" />;
 
 // New brand color additions (currently skip even number series)
 export const Emerald = () => <Palette color="EMERALD" />;
