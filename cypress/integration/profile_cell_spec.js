914b2032ff1fcaca4889bb5f1e2ed1be2de7b818
brianCollinsUI <brian@userinterviews.com>
2021-11-04 22:40:11 -0600

    Create profile_cell_spec.js

---
+++ cypress/integration/profile_cell_spec.js
@@ -0,0 +1,24 @@
+const profileCells = [
+  {
+    name: 'Small',
+    path: 'design-system-profile-cell--small',
+  },
+  {
+    name: 'Large',
+    path: 'design-system-profile-cell--large',
+  },
+  {
+    name: 'With Image',
+    path: 'design-system-profile-cell--with-image',
+  },
+];
+
+describe('Profile Cell', () => {
+  profileCells.forEach((test) => {
+    it(test.name, () => {
+      cy.visit(test.path);
+      cy.get('#storybook-preview-iframe').iframe().find('.ProfileCell').should('exist');
+      cy.percySnapshot(test.path);
+    });
+  });
+});
