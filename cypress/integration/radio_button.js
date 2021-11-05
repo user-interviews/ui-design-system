8c75f6eb798199d7e2eb0885af8318a1a92d3d49
brianCollinsUI <brian@userinterviews.com>
2021-11-04 22:53:47 -0600

    Create radio_button.js

---
+++ cypress/integration/radio_button.js
@@ -0,0 +1,20 @@
+const radioButtons = [
+  {
+    name: 'Defaut',
+    path: 'design-system-profile-radiobutton--default',
+  },
+  {
+    name: 'With Description',
+    path: 'design-system-profile-radiobutton--with-description',
+  },
+];
+
+describe('Radio Button', () => {
+  radioButtons.forEach((test) => {
+    it(test.name, () => {
+      cy.visit(test.path);
+      cy.get('#storybook-preview-iframe').iframe().find('#root').should('exist');
+      cy.percySnapshot(test.path);
+    });
+  });
+});
