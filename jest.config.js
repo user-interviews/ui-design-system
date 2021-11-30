169c67856792b0f47265bb687724b8b3c2c9010d
Jane Sebastian <jane@userinterviews.com>
2021-11-29 20:43:25 -0800

    spec_helper in spec directory

--- jest.config.js
+++ jest.config.js
@@ -129,7 +129,7 @@ module.exports = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
-    '<rootDir>/spec_helper.js'
+    '<rootDir>/spec/spec_helper.js'
   ],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
