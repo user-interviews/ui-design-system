a8733f138d2f7f6fbf764ad7fa43a6b61b431058
Jason Basuil <basuilj@gmail.com>
2021-12-06 13:49:33 -0800

    chore/UIDS-476 documentation updates (#486)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,4 @@
 import React from 'react';
 import { addDecorator } from "@storybook/react";
-import  { withA11y } from "@storybook/addon-a11y";
 
 addDecorator(story => <div style={{ padding: '1rem' }}>{story()}</div>);
-addDecorator(withA11y);
