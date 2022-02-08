6082b04452eb0e72871d1c84a26f46f98ff56d9b
Jason Basuil <basuilj@gmail.com>
2022-02-07 16:24:45 -0800

    adds flexibility for varying content length

--- cypress/integration/steps_spec.js
+++ cypress/integration/steps_spec.js
@@ -1,7 +1,20 @@
+const steps = [
+  {
+    name: 'Default',
+    path: 'components-steps--default',
+  },
+  {
+    name: 'Varying Content Length',
+    path: 'components-steps--varying-content-length',
+  },
+];
+
 describe('Steps', () => {
-  it('Default', () => {
-    cy.visit('components-steps--default');
-    cy.get('#storybook-preview-iframe').iframe().find('.Steps').should('exist');
-    cy.takePercySnapshot('components-steps--default');
+  steps.forEach((test) => {
+    it(test.name, () => {
+      cy.visit(test.path);
+      cy.get('#storybook-preview-iframe').iframe().find('.Steps').should('exist');
+      cy.takePercySnapshot(test.path);
+    });
   });
 });
--- src/Steps/Steps.stories.jsx
+++ src/Steps/Steps.stories.jsx
@@ -34,7 +34,49 @@ export const Default = () => (
       circleTextColor={colors.UX_EMERALD}
       text={(
         <>
-          <b>Apply</b> to studies. We'll notify you if you qualify!
+          <b>Confirm</b> your time slot, participate, and get paid!
+        </>
+      )}
+    />
+    <Step
+      circleBorderColor={colors.UX_EMERALD}
+      circleText="3"
+      circleTextColor={colors.UX_EMERALD}
+      text={(
+        <>
+          <b>Something</b> else...
+        </>
+      )}
+    />
+  </Steps>
+);
+
+export const VaryingContentLength = () => (
+  <Steps>
+    <Step
+      circleBorderColor={colors.UX_EMERALD}
+      circleText="1"
+      circleTextColor={colors.UX_EMERALD}
+      text={(
+        <>
+          <b>Create</b> a profile and browse studies that interest you.
+        </>
+      )}
+    />
+    <Step
+      circleBorderColor={colors.UX_EMERALD}
+      circleText="2"
+      circleTextColor={colors.UX_EMERALD}
+      text={(
+        <>
+          <b>Lorem</b> ipsum dolor sit amet, consectetur adipiscing elit. Vivamus posuere
+          vestibulum velit, interdum lacinia quam tincidunt eget. Fusce sagittis luctus massa,
+          vitae posuere odio imperdiet ac. Nam sem leo, blandit a orci vel, lacinia volutpat
+          libero. Etiam maximus nulla sed eleifend molestie. Aliquam et posuere justo, id
+          suscipit mauris. Sed luctus tortor nec accumsan commodo. Nunc in lacus pretium,
+          condimentum lorem sit amet, hendrerit arcu. Quisque gravida ullamcorper magna,
+          at hendrerit purus viverra vel. Donec tempor purus dui, nec pulvinar est vestibulum a.
+          Nam condimentum facilisis orci, nec elementum justo semper sit amet.
         </>
       )}
     />
@@ -45,6 +87,16 @@ export const Default = () => (
       text={(
         <>
           <b>Confirm</b> your time slot, participate, and get paid!
+        </>
+      )}
+    />
+    <Step
+      circleBorderColor={colors.UX_EMERALD}
+      circleText="4"
+      circleTextColor={colors.UX_EMERALD}
+      text={(
+        <>
+          <b>Something</b> else...
         </>
       )}
     />
