7a4bfb030cf7a66f7e59b3dd10b03075379a65a6
Jason Basuil <basuilj@gmail.com>
2022-03-04 17:05:14 -0800

    use DropdownToggle as basis for icon-only dropdown. Removes DropdownIconToggle.

--- src/Dropdown/index.js
+++ src/Dropdown/index.js
@@ -2,12 +2,10 @@ import Dropdown from './Dropdown';
 import DropdownToggle from './DropdownToggle';
 import DropdownItem from './DropdownItem';
 import DropdownMenu from './DropdownMenu';
-import DropdownIconToggle from './DropdownIconToggle';
 
 export {
   Dropdown,
   DropdownToggle,
   DropdownItem,
   DropdownMenu,
-  DropdownIconToggle,
 };
