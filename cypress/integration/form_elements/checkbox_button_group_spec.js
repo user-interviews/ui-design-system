ca52185c76957317ddb3b12391e2da07b04b6477
brianCollinsUI <84730553+brianCollinsUI@users.noreply.github.com>
2022-06-14 08:52:12 -0700

    Fix cypress tests (#647)

--- cypress/integration/form_elements/checkbox_button_group_spec.js
+++ cypress/integration/form_elements/checkbox_button_group_spec.js
@@ -2,27 +2,32 @@ const checkboxButtonGroups = [
   {
     name: 'Default',
     path: 'components-form-elements-checkboxbuttongroup--default',
-    class: '.InputLabel',
+    class: '.FormControlLabel__label',
+    value: 'Value',
   },
   {
     name: 'Default Row',
     path: 'components-form-elements-checkboxbuttongroup--default-row',
-    class: '.InputLabel',
+    class: '.FormControlLabel__label',
+    value: 'Label',
   },
   {
     name: 'Bordered Row',
     path: 'components-form-elements-checkboxbuttongroup--bordered-row',
-    class: '.InputLabel',
+    class: '.FormControlLabel__label',
+    value: 'Label',
   },
   {
     name: 'Description',
     path: 'components-form-elements-checkboxbuttongroup--description',
-    class: '.InputLabel',
+    class: '.FormControlLabel__label',
+    value: 'Label',
   },
   {
     name: 'Description Row',
     path: 'components-form-elements-checkboxbuttongroup--description-row',
-    class: '.InputLabel',
+    class: '.FormControlLabel__label',
+    value: 'Label',
   },
 ];
 
@@ -30,7 +35,7 @@ describe('Form Elements -> CheckboxButtonGroup', () => {
   checkboxButtonGroups.forEach((test) => {
     it(test.name, () => {
       cy.visit(test.path);
-      cy.get('#storybook-preview-iframe').iframe().find(test.class).should('contain', 'Label');
+      cy.get('#storybook-preview-iframe').iframe().find(test.class).should('contain', test.value);
       cy.takePercySnapshot(test.path);
     });
   });
