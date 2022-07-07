7971a0dd1b982ed11a7be7b81092d2f7dfb9b487
Jason Basuil <basuilj@gmail.com>
2022-07-07 15:17:05 -0700

    add accordion_spec

---
+++ cypress/integration/accordion_spec.js
@@ -0,0 +1,49 @@
+const accordions = [
+  {
+    name: 'Default',
+    path: 'components-accordion--default',
+    class: '.AccordionToggle__title',
+    match: 'Accordion Toggle #1',
+  },
+  {
+    name: 'Default Open',
+    path: 'components-accordion--default-open',
+    class: '.AccordionToggle__title',
+    match: 'Accordion Toggle #1',
+  },
+  {
+    name: 'Chevron Left',
+    path: 'components-accordion--chevron-left',
+    class: '.AccordionToggle__title',
+    match: 'Accordion Toggle #1',
+  },
+  {
+    name: 'Flush',
+    path: 'components-accordion--flush',
+    class: '.AccordionToggle__title',
+    match: 'Accordion Toggle #1',
+  },
+  {
+    name: 'Separate',
+    path: 'components-accordion--separate',
+    class: '.AccordionToggle__title',
+    match: 'Accordion Toggle #1',
+  },
+  {
+    name: 'Disabled',
+    path: 'components-accordion--disabled',
+    class: '.AccordionToggle__title',
+    match: 'Accordion Toggle -- Disabled',
+  },
+];
+
+describe('Accordion', () => {
+  accordions.forEach((test) => {
+    it(test.name, () => {
+      cy.visit(test.path);
+      const match = test.match ? test.match : test.name;
+      cy.get('#storybook-preview-iframe').iframe().find(test.class).should('contain', match);
+      cy.takePercySnapshot(test.path);
+    });
+  });
+});
