e8d86627bcc250e02a767e22b60af33ad7098aa6
Jason Basuil <basuilj@gmail.com>
2022-08-22 16:35:53 -0700

    remove extra underscore on UNSAFE_className

--- src/ToggleInput/ToggleInput.jsx
+++ src/ToggleInput/ToggleInput.jsx
@@ -16,13 +16,13 @@ const ToggleInput = ({
   name,
   onToggle,
   // eslint-disable-next-line camelcase
-  UNSAFE__className,
+  UNSAFE_className,
   ...props
 }) => (
   <label
     className={classNames(
       'ToggleInput__label',
-      UNSAFE__className,
+      UNSAFE_className,
       { 'ToggleInput__label--disabled': disabled },
     )}
     htmlFor={id}
@@ -75,7 +75,7 @@ ToggleInput.propTypes = {
    The value of the name attribute of the wrapped `<input>` element.
   */
   name: PropTypes.string,
-  UNSAFE__className: PropTypes.string,
+  UNSAFE_className: PropTypes.string,
   /**
    Callback function to invoke when the user clicks on the toggle.
    The function signature should be the following: function(e) { }.
@@ -91,7 +91,7 @@ ToggleInput.defaultProps = {
   isChecked: false,
   labelLeft: false,
   name: undefined,
-  UNSAFE__className: undefined,
+  UNSAFE_className: undefined,
   onToggle: undefined,
 };
 
