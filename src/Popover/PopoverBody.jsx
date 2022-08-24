6ea2164f748c2d2dcbdcb08b42acbce4f971c83b
Jason Basuil <basuilj@gmail.com>
2022-08-24 14:29:16 -0700

    add card variant to PopoverBody

--- src/Popover/PopoverBody.jsx
+++ src/Popover/PopoverBody.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 import { Popover as RBPopover } from 'react-bootstrap';
 
-const PopoverBody = ({ children, props }) => (
-  <RBPopover.Body {...props}>
+import './PopoverBody.scss';
+
+const PopoverBody = ({
+  children,
+  className,
+  variant,
+  ...props
+}) => (
+  <RBPopover.Body
+    className={classNames(
+      className,
+      'PopoverBody',
+      { 'PopoverBody--card': variant === 'card' },
+    )}
+    {...props}
+  >
     { children }
   </RBPopover.Body>
   );
+
+PopoverBody.propTypes = {
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(['card']),
+};
+
+PopoverBody.defaultProps = {
+  className: undefined,
+  variant: undefined,
+};
 
 export default PopoverBody;
