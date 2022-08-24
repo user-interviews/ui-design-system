b1cdeedce3ef14833fe020f77a8ed8e68189ac0c
Jason Basuil <basuilj@gmail.com>
2022-08-24 14:30:33 -0700

    update Popover, cleanup stories, add docs

--- src/Popover/Popover.stories.jsx
+++ src/Popover/Popover.stories.jsx
@@ -1,32 +1,75 @@
 import React from 'react';
 
 import Button from 'src/Button';
+import FormGroup from 'src/FormGroup';
+import Input from 'src/Input';
 
 import { OverlayTrigger } from 'src/OverlayTrigger';
 import { Popover, PopoverBody } from 'src/Popover';
 
+import mdx from './Popover.mdx';
+
 export default {
   title: 'Components/Popover',
   component: Popover,
+  subcomponents: {
+    PopoverBody,
+  },
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
 };
 
-const popover = (props) => (
-  <Popover id="popover-basic" {...props}>
-    <PopoverBody>
-      <div style={{ fontSize: '0.875rem' }}>
-        <div style={{ fontSize: '1rem', fontWeight: '700' }}>Filter on participants</div>
-        <ul>
-          <li>Automate scheduling, messages, and annoying logistics</li>
-          <li>Build your own panel to recruit for user research projects</li>
-          <li>Track participant research activity in one place</li>
-        </ul>
-      </div>
-    </PopoverBody>
-  </Popover>
+export const Default = () => (
+  <OverlayTrigger
+    overlay={(
+      <Popover id="popover-basic">
+        <PopoverBody>
+          The is the default Popover body.
+        </PopoverBody>
+      </Popover>
+    )}
+    placement="bottom"
+    trigger="click"
+  >
+    <Button variant="primary">Filter</Button>
+  </OverlayTrigger>
 );
 
-export const Default = () => (
-  <OverlayTrigger overlay={popover} placement="bottom" trigger="click">
+export const Card = () => (
+  <OverlayTrigger
+    overlay={(
+      <Popover id="popover-basic" style={{ minWidth: '30rem' }}>
+        <PopoverBody variant="card">
+          <h3 style={{ fontSize: '1rem', fontWeight: '700', marginBottom: '1.5rem' }}>Popover with Card styling</h3>
+          <FormGroup
+            label="Last project launched"
+            labelHtmlFor="last-project-launched-input"
+          >
+            <Input
+              id="last-project-launched-input"
+              name="last-project-launched"
+              placeholder="MM/DD/YYYY"
+            />
+          </FormGroup>
+          <FormGroup
+            label="Last applied date"
+            labelHtmlFor="last-applied-date-input"
+          >
+            <Input
+              id="last-applied-date-input"
+              name="last-applied-date"
+              placeholder="MM/DD/YYYY"
+            />
+          </FormGroup>
+        </PopoverBody>
+      </Popover>
+    )}
+    placement="bottom"
+    trigger="click"
+  >
     <Button variant="primary">Filter</Button>
   </OverlayTrigger>
 );
