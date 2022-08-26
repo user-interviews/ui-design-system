92bce47e101a904400d58712142d170d6876c017
Jason Basuil <basuilj@gmail.com>
2022-08-26 14:35:56 -0700

    remove prop spread on Accordion and ToggleInput, expose additional props as needed

--- src/Accordion/AccordionItem.jsx
+++ src/Accordion/AccordionItem.jsx
@@ -7,30 +7,38 @@ import RBAccordionItem from 'react-bootstrap/AccordionItem';
 import './AccordionItem.scss';
 
 const AccordionItem = ({
+  as,
   borderless,
   children,
   // eslint-disable-next-line camelcase
   UNSAFE_className,
-  ...props
 }) => (
   <RBAccordionItem
+    as={as}
     className={classNames(
       UNSAFE_className,
       'AccordionItem',
       borderless && 'AccordionItem--borderless',
     )}
-    {...props}
   >
     { children }
   </RBAccordionItem>
 );
 
 AccordionItem.propTypes = {
+  /**
+    Sets a custom element for this component
+  */
+  as: PropTypes.elementType,
+  /**
+    Removes border from accordion item
+  */
   borderless: PropTypes.bool,
   UNSAFE_className: PropTypes.string,
 };
 
 AccordionItem.defaultProps = {
+  as: undefined,
   borderless: undefined,
   UNSAFE_className: undefined,
 };
