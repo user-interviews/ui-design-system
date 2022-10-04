fe53039fc55f4dd59b104f60fd551b6aba63193c
Jason Basuil <basuilj@gmail.com>
2022-10-04 09:57:10 -0700

    change width to max-width on Card (#756)

--- src/Card/Card.stories.jsx
+++ src/Card/Card.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  withKnobs, text, radios, boolean,
+  withKnobs, text, select, boolean,
 } from '@storybook/addon-knobs';
 
 import Card, { CardSizes } from 'src/Card';
@@ -19,15 +19,49 @@ export default {
 
 export const Default = () => (
   <Card
-    divided={boolean('With divider', false)}
-    helperText={text('Helper text', '(helper text)')}
-    noPadding={boolean('Without padding', false)}
-    size={radios('Message Type', CardSizes, CardSizes.LARGE)}
-    subTitle={text('Subtitle', 'And a subtitle')}
-    title={text('Title', 'Large card with title')}
+    divided={boolean('divided', false)}
+    helperText={text('helperText', '(helper text)')}
+    noPadding={boolean('noPadding', false)}
+    size={select('size', CardSizes, undefined)}
+    subTitle={text('subTitle', 'Subtitle')}
+    title={text('title', 'Default card title')}
   >
     <div>
       Use knobs to try out the different card sizes
     </div>
   </Card>
+);
+
+export const Sizes = () => (
+  <>
+    <Card
+      size={CardSizes.EXTRA_SMALL}
+      title="xs"
+    >
+      <code>CardSizes.EXTRA_SMALL</code>
+    </Card>
+    <Card
+      size={CardSizes.SMALL}
+      title="sm"
+    >
+      <code>CardSizes.SMALL</code>
+    </Card>
+    <Card
+      size={CardSizes.MEDIUM}
+      title="md"
+    >
+      <code>CardSizes.MEDIUM</code>
+    </Card>
+    <Card
+      size={CardSizes.LARGE}
+      title="lg"
+    >
+      <code>CardSizes.LARGE</code>
+    </Card>
+    <Card
+      title="default"
+    >
+      When no size is given, the Card takes up the full width of its parent container.
+    </Card>
+  </>
 );
