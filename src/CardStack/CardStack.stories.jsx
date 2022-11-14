3ff97cd9faecc107907f9b852be7d80837ab2237
Jason Basuil <basuilj@gmail.com>
2022-11-14 15:43:48 -0800

    use Flex in CardStack centered story

--- src/CardStack/CardStack.stories.jsx
+++ src/CardStack/CardStack.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Card, { CardSizes } from 'src/Card';
 import { withKnobs, select } from '@storybook/addon-knobs';
+import Flex from 'src/Flex';
 import CardStack from './CardStack';
 
 import mdx from './CardStack.mdx';
@@ -30,7 +31,7 @@ export const Default = () => (
 );
 
 export const Centered = () => (
-  <div style={{ display: 'flex', justifyContent: 'center' }}>
+  <Flex container justifyContent="center">
     <CardStack size={select('size', Object.values(CardSizes), CardSizes.SMALL)}>
       <h1 style={{ fontSize: '1.5rem', fontWeight: 700 }}>CardStack</h1>
       <Card title="Card 1">
@@ -43,5 +44,5 @@ export const Centered = () => (
       <Card title="Card 2" />
       <Card title="Card 3" />
     </CardStack>
-  </div>
+  </Flex>
 );
