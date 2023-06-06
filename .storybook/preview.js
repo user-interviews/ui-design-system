943da8f705c7ef9a9495964a2c77fc5bc3dc1bd4
Jason Basuil <basuilj@gmail.com>
2023-06-06 11:59:26 -0700

    add storybook/addon-backgrounds

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,28 @@
+import colors from '../src/Styles/colors/palette';
+
 const preview = {
   parameters: {
+    backgrounds: {
+      default: '$ux-white',
+      values: [
+        {
+          name: '$ux-white',
+          value: colors.UX_WHITE,
+        },
+        {
+          name: '$ux-cream',
+          value: colors.UX_CREAM,
+        },
+        {
+          name: '$ux-emerald-600',
+          value: colors.UX_EMERALD_600,
+        },
+        {
+          name: '$ux-neutral-800',
+          value: colors.UX_NEUTRAL_800,
+        }
+      ],
+    },
     options: {
       storySort: {
         order: ['Foundations', 'Components'],
