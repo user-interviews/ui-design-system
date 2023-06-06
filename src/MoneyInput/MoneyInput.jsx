72c6848902628f8866408aeff0b39dd380763255
Jason Basuil <basuilj@gmail.com>
2023-06-05 18:14:43 -0700

    simplify exposed props, default intlConfig to USD

--- src/MoneyInput/MoneyInput.jsx
+++ src/MoneyInput/MoneyInput.jsx
@@ -20,36 +20,8 @@ MoneyInput.propTypes = {
   */
   allowNegativeValue: PropTypes.bool,
   className: PropTypes.string,
-  /**
-    Specify decimal scale for padding/trimming eg. 1.5 -> 1.50 or 1.234 -> 1.23 if decimal scale 2
-  */
-  decimalScale: PropTypes.number,
-  /**
-    Separator between integer part and fractional part of value
-  */
-  decimalSeparator: PropTypes.string,
-  /**
-    Limit length of decimals allowed
-  */
-  decimalsLimit: PropTypes.number,
   defaultValue: PropTypes.number,
-  /**
-    Disable abbreviations eg. 1k -> 1,000, 2m -> 2,000,000
-  */
-  disableAbbreviations: PropTypes.bool,
   disabled: PropTypes.bool,
-  /**
-    Disable auto adding the group separator between values, eg. 1000 -> 1,000
-  */
-  disableGroupSeparators: PropTypes.bool,
-  /**
-    Value will always have the specified length of decimals
-  */
-  fixedDecimalLength: PropTypes.number,
-  /**
-    Separator between thousand, million and billion
-  */
-  groupSeparator: PropTypes.string,
   /**
     International locale config
   */
@@ -63,18 +35,6 @@ MoneyInput.propTypes = {
   */
   placeholder: PropTypes.string,
   /**
-    Include a prefix eg. £ or $
-  */
-  prefix: PropTypes.string,
-  /**
-    Incremental value change on arrow down and arrow up key press
-  */
-  step: PropTypes.number,
-  /**
-    Include a suffix eg. € or %
-  */
-  suffix: PropTypes.string,
-  /**
     Transform the raw value from the input before parsing. Needs to return string.
   */
   transformRawValue: PropTypes.func,
@@ -87,23 +47,13 @@ MoneyInput.defaultProps = {
   allowNegativeValue: true,
   className: undefined,
   defaultValue: undefined,
+  disabled: false,
+  intlConfig: { locale: 'en-US', currency: 'USD' },
+  maxLength: undefined,
+  placeholder: undefined,
+  transformRawValue: undefined,
   value: undefined,
   onValueChange: undefined,
-  placeholder: undefined,
-  decimalsLimit: 2,
-  decimalScale: undefined,
-  fixedDecimalLength: undefined,
-  prefix: '$',
-  suffix: undefined,
-  decimalSeparator: undefined,
-  groupSeparator: undefined,
-  intlConfig: undefined,
-  disabled: false,
-  disableAbbreviations: false,
-  disableGroupSeparators: false,
-  maxLength: undefined,
-  step: undefined,
-  transformRawValue: undefined,
 };
 
 export default MoneyInput;
