5ab953da82933a4243733bac348c86916f479f0d
Jason Basuil <basuilj@gmail.com>
2023-09-15 13:30:06 -0700

    add TypeScript (#992)
    * add TypeScript packages
    
    * add tsconfig.json
    
    * update storybook/main.js
    
    * add preset-typescript to babelrc
    
    * add build-ts script
    
    * update tsconfig.json
    
    * add dummy TestPill component
    
    * Fixes up TS config (#993)
    
    * convert EmptyState to TS and remove demo TestPill
    
    * update EmptyStateProps
    
    * use relative import for Heading and Text in EmptyState
    
    * add react/jsx-filename-extension to .eslintrc
    
    * remove test copy
    
    * trailing comma
    
    * bring types/react-dom version down to ^16
    
    ---------
    
    Co-authored-by: Kyle Shike <kyleshike@gmail.com>

---
+++ src/EmptyState/EmptyState.tsx
@@ -0,0 +1,57 @@
+import React, { ReactNode } from 'react';
+
+import classNames from 'classnames';
+
+import { Heading } from '../Heading';
+import { Text } from '../Text';
+
+import './EmptyState.scss';
+
+export interface EmptyStateProps {
+  className?: string;
+  fullWidth?: boolean;
+  marginTop?: 'sm' | 'md' | 'lg' | 'none';
+  primaryAction?: ReactNode;
+  subtitle?: string | ReactNode;
+  title?: ReactNode;
+}
+
+const EmptyState = ({
+  className,
+  fullWidth = false,
+  marginTop = 'sm',
+  primaryAction,
+  subtitle,
+  title,
+}: EmptyStateProps) => (
+  <div className={classNames(
+    className,
+    'EmptyState',
+    marginTop && `EmptyState--margin-top--${marginTop}`,
+  )}
+  >
+    <div className={classNames(
+      'EmptyState__content',
+      fullWidth && 'EmptyState--full-width',
+    )}
+    >
+      {title && (
+        <Heading className="EmptyState__title" level={4} size="lg" textAlign="center">{title}</Heading>
+      )}
+
+      {subtitle && (
+        <Text className="EmptyState__subtitle" textAlign="center">{subtitle}</Text>
+      )}
+
+      {primaryAction && (
+        <div className="EmptyState__actions">
+          <div className="EmptyState__actions__primary-action">
+            {primaryAction}
+          </div>
+        </div>
+      )}
+    </div>
+  </div>
+);
+
+export default EmptyState;
