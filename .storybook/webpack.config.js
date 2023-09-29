21a33c671df7b5ada76c6cda38816229db9a614d
Kyle Shike <kyle.s@userinterviews.com>
2023-09-29 15:58:15 -0400

    updates ts eslint tooling

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,7 +3,16 @@ const path = require('path');
 module.exports = function({ config }) {
   config.module.rules.push({
     test: /\.scss$/,
-    use: ['style-loader', 'css-loader', 'sass-loader'],
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      },
+      'sass-loader'
+    ],
     include: path.resolve(__dirname, '../')
   });
 
