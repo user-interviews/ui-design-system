b359019600283e806a3c85d3c668add84b405400
Jason Basuil <basuilj@gmail.com>
2023-12-07 11:47:51 -0800

    add prefix and step props

--- src/MoneyInput/MoneyInput.stories.jsx
+++ src/MoneyInput/MoneyInput.stories.jsx
@@ -47,3 +47,78 @@ Default.args = {
   placeholder: 'Please enter a number',
   decimalsLimit: 2,
 };
+
+export const Step = (args) => {
+  const [value, setValue] = useState(200);
+
+  const handleOnValueChange = (val) => {
+    if (!val) {
+      setValue('');
+      return;
+    }
+    setValue(val);
+  };
+
+  return (
+    <>
+      <FormGroup
+        helperText="Use the Up Arrow (↑) or Down Arrow (↓) to adjust the incremental value change"
+        label="Incentive amount"
+        labelHtmlFor="money-input-2"
+      >
+        <MoneyInput
+          value={value}
+          onValueChange={handleOnValueChange}
+          {...args}
+        />
+        <br />
+        <Heading>Value: {value}</Heading>
+      </FormGroup>
+    </>
+  );
+};
+
+Step.args = {
+  decimalsLimit: 2,
+  id: 'money-input-2',
+  placeholder: 'Please enter a number',
+  step: 1,
+};
+
+export const Prefix = (args) => {
+  const [value, setValue] = useState(500);
+
+  const handleOnValueChange = (val) => {
+    if (!val) {
+      setValue('');
+      return;
+    }
+    setValue(val);
+  };
+
+  return (
+    <>
+      <FormGroup
+        helperText="Change the prefix"
+        label="Incentive amount"
+        labelHtmlFor="money-input-3"
+      >
+        <MoneyInput
+          value={value}
+          onValueChange={handleOnValueChange}
+          {...args}
+        />
+        <br />
+        <Heading>Value: {value}</Heading>
+      </FormGroup>
+    </>
+  );
+};
+
+Prefix.args = {
+  decimalsLimit: 2,
+  id: 'money-input-3',
+  placeholder: 'Please enter a number',
+  prefix: 'USD$ ',
+  step: 1,
+};
