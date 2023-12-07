cb76a250156d783d88d793dec0e2ad6c5d7fa6c4
Jason Basuil <basuilj@gmail.com>
2023-12-07 15:08:55 -0800

    give default sizes to levels, make weight and size optional

--- src/Heading/Heading.tsx
+++ src/Heading/Heading.tsx
@@ -8,16 +8,27 @@ export interface HeadingProps {
   children?: ReactNode;
   className?: string;
   level: 1 | 2 | 3 | 4 | 5 | 6;
-  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl';
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl';
   textAlign?: 'left' | 'center' | 'right';
-  weight: 'regular' | 'medium' | 'bold';
+  weight?: 'regular' | 'medium' | 'bold';
 }
+
+const LevelToSizeMap = {
+  1: 'xxxl',
+  2: 'xxl',
+  3: 'xl',
+  4: 'lg',
+  5: 'md',
+  6: 'sm',
+};
+
+const getHeadingSizeClassFromLevel = (level: number) => `Heading--${LevelToSizeMap[level]}`;
 
 const Heading = ({
   children,
   className,
-  level = 2,
-  size = 'xxl',
+  level = 1,
+  size,
   textAlign,
   weight = 'bold',
   ...props
@@ -28,6 +39,7 @@ const Heading = ({
       className: classNames(
         className,
         'Heading',
+        !size && getHeadingSizeClassFromLevel(level),
         {
           [`Heading--${size}`]: !!size,
           [`Heading--${weight}`]: !!weight,
