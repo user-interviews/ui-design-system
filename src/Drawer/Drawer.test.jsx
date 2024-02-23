2c1e01603360a03c6171867d01101b1eb7aef9c2
Dominik Schab <dominik@userinterviews.com>
2024-02-23 11:30:13 -0500

    add before each to clean up classList on body

--- src/Drawer/Drawer.test.jsx
+++ src/Drawer/Drawer.test.jsx
@@ -30,18 +30,72 @@ const elements = {
   },
 };
 
-describe('Drawer', () => {
-  function SetupDrawerWithChildren(props) {
-    const defaultProps = {
-      onRequestClose: () => {},
-    };
+function SetupDrawerWithChildren(props) {
+  const defaultProps = {
+    onRequestClose: () => {},
+  };
 
-    return (
-      <Drawer {...defaultProps} {...props}>
-        <div>children</div>
+  return (
+    <Drawer {...defaultProps} {...props}>
+      <div>children</div>
+    </Drawer>
+  );
+}
+
+function SetupMultipleDrawersWithOneOpen() {
+  const [isDrawerOneVisible, setIsDrawerOneVisible] = useState(true);
+  const [isDrawerTwoVisible, setIsDrawerTwoVisible] = useState(false);
+  const [isDrawerThreeVisible, setIsDrawerThreeVisible] = useState(false);
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => setIsDrawerOneVisible((prevState) => !prevState)}
+      >
+        toggle visibility drawer1
+      </button>
+
+      <button
+        type="button"
+        onClick={() => setIsDrawerTwoVisible((prevState) => !prevState)}
+      >
+        toggle visibility drawer2
+      </button>
+
+      <button
+        type="button"
+        onClick={() => setIsDrawerThreeVisible((prevState) => !prevState)}
+      >
+        toggle visibility drawer3
+      </button>
+
+      <Drawer
+        visible={isDrawerOneVisible}
+        onRequestClose={() => setIsDrawerOneVisible(false)}
+      >
+        <div>children1</div>
       </Drawer>
-    );
-  }
+      <Drawer
+        visible={isDrawerTwoVisible}
+        onRequestClose={() => setIsDrawerTwoVisible(false)}
+      >
+        <div>children2</div>
+      </Drawer>
+      <Drawer
+        visible={isDrawerThreeVisible}
+        onRequestClose={() => setIsDrawerThreeVisible(false)}
+      >
+        <div>children3</div>
+      </Drawer>
+    </div>
+  );
+}
+
+describe('Drawer', () => {
+  beforeEach(() => {
+    window.document.body.classList.remove(...window.document.body.classList);
+  });
 
   describe('When component renders single drawer', () => {
     describe('when visible is false', () => {
@@ -144,79 +198,46 @@ describe('Drawer', () => {
       });
     });
   });
-});
 
-describe('Multiple Drawers', () => {
-  function SetupMultipleDrawersWithOneOpen() {
-    const [isDrawerOneVisible, setIsDrawerOneVisible] = useState(true);
-    const [isDrawerTwoVisible, setIsDrawerTwoVisible] = useState(false);
-    const [isDrawerThreeVisible, setIsDrawerThreeVisible] = useState(false);
+  describe('When component renders multiple Drawers', () => {
 
-    return (
-      <div>
-        <button
-          type="button"
-          onClick={() => setIsDrawerOneVisible((prevState) => !prevState)}
-        >
-          toggle visibility drawer1
-        </button>
-
-        <button
-          type="button"
-          onClick={() => setIsDrawerTwoVisible((prevState) => !prevState)}
-        >
-          toggle visibility drawer2
-        </button>
-
-        <button
-          type="button"
-          onClick={() => setIsDrawerThreeVisible((prevState) => !prevState)}
-        >
-          toggle visibility drawer3
-        </button>
-
-        <Drawer
-          visible={isDrawerOneVisible}
-          onRequestClose={() => setIsDrawerOneVisible(false)}
-        >
-          <div>children1</div>
-        </Drawer>
-        <Drawer
-          visible={isDrawerTwoVisible}
-          onRequestClose={() => setIsDrawerTwoVisible(false)}
-        >
-          <div>children2</div>
-        </Drawer>
-        <Drawer
-          visible={isDrawerThreeVisible}
-          onRequestClose={() => setIsDrawerThreeVisible(false)}
-        >
-          <div>children3</div>
-        </Drawer>
-      </div>
-    );
-  }
-
-  describe('When component renders multiple drawers with drawer1 visible', () => {
-    it('body tag has Drawer__Body--open', () => {
-      const { container } = render(<SetupMultipleDrawersWithOneOpen />);
-      const body = container.closest('body');
-
-      expect(body.classList).toContain('Drawer__Body--open');
-    });
-
-    describe('when user clicks on drawer1 toggle visibility button', () => {
-      it('body tag does not have Drawer__body--open', () => {
+    describe('When component renders multiple drawers with drawer1 visible', () => {
+      it('body tag has Drawer__Body--open', () => {
         const { container } = render(<SetupMultipleDrawersWithOneOpen />);
         const body = container.closest('body');
 
         expect(body.classList).toContain('Drawer__Body--open');
+      });
 
-        userEvent.click(elements.drawerOneToggleVisibilityButton.get());
+      describe('when user clicks on drawer1 toggle visibility button', () => {
+        it('body tag does not have Drawer__body--open', () => {
+          const { container } = render(<SetupMultipleDrawersWithOneOpen />);
+          const body = container.closest('body');
 
-        expect(body.classList).not.toContain('Drawer__Body--open');
+          expect(body.classList).toContain('Drawer__Body--open');
+
+          userEvent.click(elements.drawerOneToggleVisibilityButton.get());
+
+          expect(body.classList).not.toContain('Drawer__Body--open');
+        });
       });
     });
+    it.todo('add more spec variations');
   });
-  it.todo('add more spec variations');
 });
+
+// describe('when hasBackgroundOverlay is falsex', () => {
+//   it('does not have drawer overlay', () => {
+//     render(<SetupDrawerWithChildren hasBackgroundOverlay={false} visible />);
+//
+//     expect(elements.drawerOverlay.query()).not.toBeInTheDocument();
+//   });
+//
+//   it('body tag does not have Drawer__Body--openx', () => {
+//     // eslint-disable-next-line max-len
+//     const { container } = render(<SetupDrawerWithChildren hasBackgroundOverlay={false} visible />);
+//     const body = container.closest('body');
+//
+//     expect(body.classList).not.toContain('Drawer__Body--open');
+//   });
+// });
