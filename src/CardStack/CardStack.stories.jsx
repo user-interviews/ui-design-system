520074828e7f6d6c379b6da9229b31b168688a3e
Kyle Shike <kyle.s@userinterviews.com>
2024-03-27 14:46:38 -0400

    card stack

--- src/CardStack/CardStack.stories.jsx
+++ src/CardStack/CardStack.stories.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Card, { CardSizes } from 'src/Card';
-import { withKnobs, select } from '@storybook/addon-knobs';
+// import { withKnobs, select } from '@storybook/addon-knobs';
 import { Flex } from 'src/Flex';
 import CardStack from './CardStack';
 
@@ -9,40 +9,35 @@ import mdx from './CardStack.mdx';
 
 export default {
   title: 'Layouts/CardStack',
-  decorators: [withKnobs],
   component: CardStack,
   parameters: {
+    controls: { exclude: [] },
     docs: {
       page: mdx,
     },
   },
+  args: {
+    size: CardSizes.SMALL,
+  },
+  argTypes: {
+    size: {
+      options: Object.values(CardSizes),
+      mapping: CardSizes,
+      control: { type: 'select' },
+    },
+  },
 };
 
-export const Default = () => (
-  <CardStack size={select('size', Object.values(CardSizes), CardSizes.SMALL)}>
-    <h1 style={{ fontSize: '1.5rem', fontWeight: 700 }}>CardStack</h1>
-    <Card title="Card 1">
-      <p>A layout container for a vertical stack of <code>Card</code> components</p>
-      <p>Adjust the <code>size</code> knob below</p>
-    </Card>
-    <Card title="Card 2" />
-    <Card title="Card 3" />
-  </CardStack>
-);
-
-export const Centered = () => (
-  <Flex container justifyContent="center">
-    <CardStack size={select('size', Object.values(CardSizes), CardSizes.SMALL)}>
+export const Default = {
+  render: (args) => (
+    <CardStack {...args}>
       <h1 style={{ fontSize: '1.5rem', fontWeight: 700 }}>CardStack</h1>
       <Card title="Card 1">
-        <p>Many pages consist of <code>CardStack</code> containers centered on the page.</p>
-        <p>
-          <code>CardStack</code> can be placed in any container and aligned as needed.
-          This example is in a container with <code>display: flex; justify-content: center;</code>
-        </p>
+        <p>A layout container for a vertical stack of <code>Card</code> components</p>
+        <p>Adjust the <code>size</code> knob below</p>
       </Card>
       <Card title="Card 2" />
       <Card title="Card 3" />
     </CardStack>
-  </Flex>
-);
+  ),
+};
