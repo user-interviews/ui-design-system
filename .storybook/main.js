8e5546c3c240b2b4d6ad88ceb4a6a883c2bbfba0
Kyle Shike <kyleshike@gmail.com>
2024-04-01 14:46:40 -0400

    Upgrade Storybook to v8.0.5 (#1179)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,39 +1,45 @@
 const config = {
-  addons: [
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        jsxOptions: {
-          babelrc: true,
-          configFile: true
-        }
+  addons: [{
+    name: '@storybook/addon-docs',
+    options: {
+      jsxOptions: {
+        babelrc: true,
+        configFile: true
       }
-    },
-    '@storybook/addon-a11y',
-    '@storybook/addon-actions',
-    '@storybook/addon-backgrounds',
-    '@storybook/addon-controls',
-    '@storybook/addon-links',
-    '@storybook/addon-storysource',
-    '@storybook/addon-mdx-gfm'
-  ],
+    }
+  }, '@storybook/addon-a11y', '@storybook/addon-actions', '@storybook/addon-backgrounds', '@storybook/addon-controls', '@storybook/addon-links', '@storybook/addon-storysource', '@storybook/addon-mdx-gfm', '@storybook/addon-webpack5-compiler-babel', ({
+    name: "@storybook/addon-styling-webpack",
+
+    options: {
+      rules: [{
+    test: /\.css$/,
+    sideEffects: true,
+    use: [
+        require.resolve("style-loader"),
+        {
+            loader: require.resolve("css-loader"),
+            options: {
+                
+                
+            },
+        },
+    ],
+  },],
+    }
+  })],
   docs: {
     autodocs: true,
     defaultName: 'Docs',
   },
   stories: [
-    '../stories/Intro.stories.mdx',
+    '../stories/Intro.stories.jsx',
     '../src/**/*.stories.@(js|mdx)',
     '../src/**/*.stories.@(ts|mdx)',
     '../src/**/*.stories.js[x]',
     '../src/**/*.stories.ts[x]',
-    '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.js[x]',
     '../stories/**/*.stories.ts[x]'
   ],
-  features: {
-    storyStoreV7: false,
-  },
   framework: {
     name: '@storybook/react-webpack5',
     options: { fastRefresh: true },
