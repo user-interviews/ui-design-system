5c87a8ee38c797c0900e9f06adf1aa148446adce
Kyle Shike <kyle.s@userinterviews.com>
2024-04-29 10:40:59 -0400

    iconcell

--- src/IconCell/IconCell.tsx
+++ src/IconCell/IconCell.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import propTypes from 'prop-types';
 
+import { type IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './IconCell.scss';
 
+type IconCellProps = {
+  icon: IconDefinition;
+};
+
 const IconCell = ({
   icon,
-}) => (
+}: IconCellProps) => (
   <div className="IconCell">
     <FontAwesomeIcon icon={icon} />
   </div>
 );
-
-IconCell.propTypes = {
-  icon: propTypes.object.isRequired,
-};
 
 export default IconCell;
