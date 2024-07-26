c08ff0d74198aae352db72c6eef0be5dea032b79
Dominik Schab <dominik@userinterviews.com>
2024-07-26 07:50:27 -0400

    test

--- src/RichTextEditor/RichTextEditor.tsx
+++ src/RichTextEditor/RichTextEditor.tsx
@@ -98,7 +98,7 @@ export type RichTextEditorProps = {
   */
   onChange: (arg0: string) => void;
   /**
-   Optional callback function to call when RichTextEditor is in display mode truncates text
+   Optional callback function to call when RichTextEditor is in display mode truncates tex
    */
   onTruncate?: () => void;
 }
