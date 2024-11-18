8dad39dab425fdb107e8489b7e19d7d941855b22
Jeff Baxendale <jeffbax@users.noreply.github.com>
2024-11-18 15:11:16 -0500

    comment out broken var references

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,24 +14,24 @@ const preview = {
   parameters: {
     backgrounds: {
       default: '--ux-white',
-      values: [
-        {
-          name: '--ux-white',
-          value: colors.UX_WHITE,
-        },
-        {
-          name: '--ux-cream',
-          value: colors.UX_CREAM,
-        },
-        {
-          name: '--ux-emerald-600',
-          value: colors.UX_EMERALD_600,
-        },
-        {
-          name: '--ux-neutral-800',
-          value: colors.UX_NEUTRAL_800,
-        }
-      ],
+      // values: [
+      //   {
+      //     name: '--ux-white',
+      //     value: colors.UX_WHITE,
+      //   },
+      //   {
+      //     name: '--ux-cream',
+      //     value: colors.UX_CREAM,
+      //   },
+      //   {
+      //     name: '--ux-emerald-600',
+      //     value: colors.UX_EMERALD_600,
+      //   },
+      //   {
+      //     name: '--ux-neutral-800',
+      //     value: colors.UX_NEUTRAL_800,
+      //   }
+      // ],
     },
     options: {
       storySort: {
--- src/Styles/colors/inputs.js
+++ src/Styles/colors/inputs.js
@@ -1,7 +1,7 @@
 import palette from './palette';
 
 export default {
-  inputBorderColor: palette.UX_GRAY_400,
-  inputDisabledBg: palette.UX_GRAY_300,
+  // inputBorderColor: palette.UX_GRAY_400,
+  // inputDisabledBg: palette.UX_GRAY_300,
   selectOptionFocusedBg: '#C7DDEF',
 };
