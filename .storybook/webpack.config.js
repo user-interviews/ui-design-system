3793e8adfa09447d40c32461a53c7c782ae9ef8a
Jeff Baxendale <jeff@userinterviews.com>
2025-10-31 11:21:10 -0400

    support codecov bundle analysis
    # Conflicts:
    #	package.json
    #	yarn.lock

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,16 @@
 const path = require('path');
+const { codecovWebpackPlugin } = require("@codecov/webpack-plugin");
 
 module.exports = function({ config }) {
+  config.plugins = config.plugins || [];
+  config.plugins.push(
+    codecovWebpackPlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "design-system",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  );
+
   config.module.rules.push({
     test: /\.scss$/,
     use: [
