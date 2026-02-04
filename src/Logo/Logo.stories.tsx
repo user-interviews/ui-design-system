c41a06589ee5767cf7db9608021249ad134ac3b8
Jason Basuil <basuilj@gmail.com>
2026-02-04 15:33:19 -0800

    update import

--- src/Logo/Logo.stories.tsx
+++ src/Logo/Logo.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
 import uiLogo from './ui_logo.svg';
 import { Logo } from './Logo';
