6a4561188efaa552681c1c83baa7165d8dd9da21
Jason Basuil <basuilj@gmail.com>
2026-02-04 12:42:52 -0800

    @storybook/addon-mcp (#1389)

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,28 +7,40 @@ const config = {
         configFile: true
       }
     }
-  }, '@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-webpack5-compiler-babel', ({
+  }, '@storybook/addon-a11y', '@storybook/addon-links', {
+    name: '@storybook/addon-mcp',
+    options: {
+      toolsets: {
+        dev: true,
+        docs: true,
+      },
+      experimentalFormat: 'markdown',
+    },
+  }, '@storybook/addon-webpack5-compiler-babel', ({
     name: "@storybook/addon-styling-webpack",
 
     options: {
       rules: [{
-    test: /\.css$/,
-    sideEffects: true,
-    use: [
-        require.resolve("style-loader"),
-        {
+        test: /\.css$/,
+        sideEffects: true,
+        use: [
+          require.resolve("style-loader"),
+          {
             loader: require.resolve("css-loader"),
             options: {
-                
-                
+
+
             },
-        },
-    ],
-  },],
+          },
+        ],
+      },],
     }
   })],
   docs: {
     defaultName: 'Docs'
+  },
+  features: {
+    experimentalComponentsManifest: true,
   },
   stories: [
     '../stories/Intro.stories.jsx',
